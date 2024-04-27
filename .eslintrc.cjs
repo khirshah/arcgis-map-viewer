2ea0303364aa3e74c5ff6de16b02c20b2da470df
agi <khirshah@gmail.com>
2024-04-27 15:02:57 +0100

    add more eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    "semi": [1, "never"],
+    'semi': [1, "never"],
+    'eol-last': [1, "always"],
+    'object-curly-spacing': [1, 'always'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
