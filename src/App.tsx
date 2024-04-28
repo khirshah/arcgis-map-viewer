2c1bfc6d7ca7b7b000a180eb231acc92c76aac5f
agi <khirshah@gmail.com>
2024-04-28 23:52:36 +0100

    Add React router

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,42 @@ import Page from "./components/common/Page/Page.tsx"
 import { Header, NewZealandMap, SantaMonicaMap } from "./components"
 import mapData from "./data/mapsInfo.json"
 import esriConfig from "@arcgis/core/config"
+import {
+  createBrowserRouter, createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
 esriConfig.apiKey = import.meta.env.VITE_ARCGIS_API_KEY
 
-function App() {
+const CurrentApp = () => {
+  return <div className="app">
+    <Header/>
+    <Page
+      mapData={mapData.newZealandMap}
+      MapContent={NewZealandMap}
+    />
+    <Page
+      mapData={mapData.santaMonicaMap}
+      MapContent={SantaMonicaMap}
+    />
+  </div>
+}
+
+const AppRoutes = () => {
 
   return (
-    <div className="app">
-      <Header/>
-      <Page
-        mapData={mapData.newZealandMap}
-        MapContent={NewZealandMap}
-      />
-      <Page
-        mapData={mapData.santaMonicaMap}
-        MapContent={SantaMonicaMap}
-      />
-    </div>
+    <>
+    <Route
+      path='/'
+      element={<CurrentApp />}
+    />
+    </>
   )
 }
+
+const router = createBrowserRouter(createRoutesFromElements(AppRoutes()))
+
+const App = () => <RouterProvider router={router}/>
 
 export default App
